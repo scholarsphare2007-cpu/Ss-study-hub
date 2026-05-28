2eb8b7d94e74fd1b3d9de1f1fb8e567b48df5083
scholarsphare2007-cpu <scholarsphare2007@gmail.com>
2026-05-28 09:22:21 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,32 +4,168 @@ const resourcesData = [
         course: "B.Sc",
         semester: "2nd Sem",
         subject: "Mathematics",
+        type: "Syllabus",
+        title: "Mathematics 2nd Syllabus (2025-26)",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Mathematics", 
         type: "Notes",
         title: "Matrices & Calculus - Unit 1",
-        link: "#" // Yahan apni Google Drive ka download link daal dena
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Mathematics",
+        type: "Notes",
+        title: "Matrices & Calculus - Unit 2",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Mathematics",
+        type: "Notes",
+        title: "Matrices & Calculus - Unit 3",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Mathematics",
+        type: "Notes",
+        title: "Matrices & Calculus - Unit 4",
+        link: "#"
     },
     {
         course: "B.Sc",
         semester: "2nd Sem",
         subject: "Physics",
-        type: "Practical File",
-        title: "Optics Fully Solved Practical File",
+        type: "Syllabus",
+        title: "Physics 2nd sem syllabus (2025-26)",
+        link: "https://drive.google.com/file/d/1-dU37HUpf8ZCSFD9z9XX99QYMh7grFpO/view?usp=drive_link"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Physics",
+        type: "Notes",
+        title: "Physics Notes unit-1",
+        link: "https://drive.google.com/file/d/1K15rmp37ZQQ-061KHdxQNbDC0W_zlVkL/view?usp=sharing"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Physics",
+        type: "Notes",
+        title: "Physics Notes unit-2",
         link: "#"
     },
     {
-        course: "B.Com",
-        semester: "1st Sem",
-        subject: "Accountancy",
-        type: "PYQ",
-        title: "Financial Accounting 2025 Exam Paper",
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Physics",
+        type: "Notes",
+        title: "Physics Notes unit-3",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Physics",
+        type: "Notes",
+        title: "Physics Notes unit-4",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Physics",
+        type: "Practical-Files",
+        title: "Physics lab file 2nd sem",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Chemistry",
+        type: "Syllabus",
+        title: "Chemistry 2nd sem syllabus (2025-26)",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Chemistry",
+        type: "Notes",
+        title: "Chemistry Notes unit-1",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Chemistry",
+        type: "Notes",
+        title: "Chemistry Notes unit-2",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Chemistry",
+        type: "Notes",
+        title: "Chemistry Notes unit-3",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Chemistry",
+        type: "Notes",
+        title: "Chemistry Notes unit-4",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "Chemistry",
+        type: "Practical-Files",
+        title: "Chemistry lab file 2nd sem",
         link: "#"
     },
     {
         course: "B.Sc",
         semester: "2nd Sem",
         subject: "General English",
-        type: "Assignment File",
-        title: "General English Assignment Theory",
-        link: "https://drive.google.com/file/d/1MfcNLSeaQLjrjq_qV04RcTdONqDfblet/view?usp=drive_link"
-    }
+        type: "Syllabus",
+        title: "General English Syllabus (2025-26)",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "General English",
+        type: "Notes",
+        title: "General English Notes",
+        link: "#"
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "General Hindi",
+        type: "Syllabus",
+        title: "General Hindi Syllabus (2025-26) ",
+        link: "#" // Yahan apni Google Drive ka download link daal dena
+    },
+    {
+        course: "B.Sc",
+        semester: "2nd Sem",
+        subject: "General Hindi",
+        type: "Notes",
+        title: "General Hindi Notes ",
+        link: "#" // Yahan apni Google Drive ka download link daal dena
+    },
 ];
